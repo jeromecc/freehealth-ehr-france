a8957586a9bc50cf91e1013963b46dbfd1a26533
Jerome Pinguet <jerome@jerome.cc>
2016-12-16 05:21:54 +0100

    final version of fr lab test prescription

--- subforms/prescriptions/lab_assistant/scripts/xx/bizone.js
+++ subforms/prescriptions/lab_assistant/scripts/xx/bizone.js
@@ -1,6 +1,6 @@
-namespace.module('com.freemedforms.prescriptions.labtests.fr.bizone', function (exports, require) {
-    var bz_affectionExoneranteHeaderTextEdit;
-    var bz_maladiesIntercurrentesHeaderTextEdit;
+namespace.module('io.freehealth.prescriptions.labtests.fr.bizone', function (exports, require) {
+    //var bz_affectionExoneranteHeaderTextEdit;
+    //var bz_maladiesIntercurrentesHeaderTextEdit;
     var bz_bizoneCheckUi;
     var bz_bizoneCheckItem;
     var orderText;
@@ -10,14 +10,16 @@ namespace.module('com.freemedforms.prescriptions.labtests.fr.bizone', function (
     var bz_affectionExoneranteUi;
     var bz_maladiesIntercurrentesUi;
     var bz_rppsLineEdit;
-    var bz_headerUi;
+    var bz_interHeaderUi;
+    var bz_exoHeaderUi;
     var bz_current;
     var bz_htmlHeader = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\"><html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">p, li { white-space: pre-wrap; }</style></head><body style=\"font-family:'Cantarell'; font-size:11pt; font-weight:400; font-style:normal;\">";
     var bz_affectionExoneranteHeader = "<br/><hr/><p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Prescriptions relatives au traitement de l'affection de longue durée reconnue (liste ou hors liste)<br />(AFFECTION EXONÉRANTE)</p><hr />";
     var bz_maladiesIntercurrentesHeader = "<hr /><p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Prescriptions SANS RAPPORT avec l'affection de longue durée<br />(MALADIES INTERCURRENTES)</p><hr />";
 
-   var bz_header ="<table border=\"1\" style=\" border-style:none; margin-top:6px; margin-bottom:6px; margin-left:6px; margin-right:6px;\" width=\"100%\" cellspacing=\"4\" cellpadding=\"4\" bgcolor=\"#ffffff\"><tr><td colspan=2 align=\"center\"><span style=\" font-family:'Sans Serif'; font-size:14pt;\">{{~User.Contact.FullAddress~}} </span><br><span style=\" font-family:'Sans Serif'; font-size:12pt;\">Tél {{~User.Contact.Tel1~}} </span><br><span style=\" font-family:'Sans Serif'; font-size:12pt;\">Fax {{~User.Contact.Fax~}} </span><br><span style=\" font-family:'Sans Serif'; font-size:12pt;\">Email {{~User.Contact.Mail~}} </span></td></tr><tr><td width=\"33%\"><span style=\" font-family:'Lucida Grande'; font-size:14pt;\">{{~User.Identity.FullName~}}</span><span style=\" font-family:'Lucida Grande'; font-size:13pt;\">{{~User.Professional.Specialties~}} </span><p><span style=\" font-family:'Code 128'; font-size:36pt;\">{{~User.Professional.Identifiants~}} </span></p><p><span style=\" font-family:'Sans Serif'; font-size:10pt;\">{{~User.Professional.Qualifications~}} </span></p></td><td colspan=\"2\"><p align=\"right\"><span style=\" font-family:'Lucida Grande'; font-size:13pt;\">[[DATE]]</span></p><p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Sans Serif'; font-size:9pt;\"><br /></p><p align=\"right\"><span style=\" font-family:'Sans Serif'; font-size:14pt;\">{{~Patient.Identity.FullName~}}</span></p><p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Lucida Grande'; font-size:13pt;\"><br /></p><p align=\"right\"><span style=\" font-family:'Sans Serif'; font-size:9pt;\">{{~Patient.Age.YearsOld~ ans}}</span></p></td></tr></table>";
-    var bz_headerItem;
+
+    var bz_interHeaderItem;
+    var bz_exoHeaderItem;
     var bz_radioItem;
     var bz_bizoneCollapsibleGroupItem;
     var bz_bizoneCollapsibleGroupUi;
@@ -29,25 +31,59 @@ namespace.module('com.freemedforms.prescriptions.labtests.fr.bizone', function (
     var bz_warningItem;
     var bz_affectionExoneranteItem;
     var bz_frequentExoUi;
+    var bz_frequentExoItem;
     var bz_hematoExoUi;
+    var bz_hematoExoItem;
     var bz_coagExoUi;
+    var bz_coagExoItem;
     var bz_electrolytesExoUi;
+    var bz_electrolytesExoItem;
     var bz_kidneyExoUi;
+    var bz_kidneyExoItem;
     var bz_protsExoUi;
+    var bz_protsExoItem;
     var bz_liverExoUi;
+    var bz_liverExoItem;
     var bz_metabolicExoUi;
+    var bz_metabolicExoItem;
     var bz_tumorExoUi;
+    var bz_tumorExoItem;
     var bz_ironExoUi;
+    var bz_ironExoItem;
     var bz_prostateExoUi;
+    var bz_prostateExoItem;
     var bz_vitaminsExoUi;
+    var bz_vitaminsExoItem;
     var bz_biermerExoUi;
+    var bz_biermerExoItem;
+    var bz_thyroidExoItem;
     var bz_thyroidExoUi;
     var bz_serologiesExoUi;
+    var bz_serologiesExoItem;
     var bz_prExoUi;
+    var bz_prExoItem;
+    //
+    var item = [bz_frequentExoItem,
+    bz_hematoExoItem,
+    bz_coagExoItem,
+    bz_electrolytesExoItem,
+    bz_kidneyExoItem,
+    bz_protsExoItem,
+    bz_liverExoItem,
+    bz_metabolicExoItem,
+    bz_tumorExoItem,
+    bz_ironExoItem,
+    bz_prostateExoItem,
+    bz_vitaminsExoItem,
+    bz_biermerExoItem,
+    bz_thyroidExoItem,
+    bz_serologiesExoItem,
+    bz_prExoItem];
 
   exports.extend({
     'setupUi': setupUi,
-    'bz_onCheckChanged': bz_onCheckChanged
+    'bz_onCheckChanged': bz_onCheckChanged,
+    'bz_getUiElements' : bz_getUiElements
   });
 
                      function setupUi() {
@@ -58,6 +94,7 @@ namespace.module('com.freemedforms.prescriptions.labtests.fr.bizone', function (
 
 
 function bz_getUiElements() {
+        //print("hello");
         freemedforms.forms.namespaceInUse = "";
         var formUi = freemedforms.forms.item("Subs::Tools::LabTests");
         freemedforms.forms.namespaceInUse = "Subs::Tools::LabTests";
@@ -67,69 +104,70 @@ function bz_getUiElements() {
         bz_affectionExoneranteItem = freemedforms.forms.item("Affection::Exonerante");
         bz_maladiesIntercurrentesItem = freemedforms.forms.item("Maladies::Intercurrentes");
         bz_maladiesIntercurrentesUi = ui.findChild("maladiesIntercurrentesTextEdit");
-        bz_headerUi = ui.findChild("headerLineEdit");
-        bz_headerItem = freemedforms.forms.item("Header");
+        bz_interHeaderUi = ui.findChild("interHeaderLineEdit");
+        bz_interHeaderItem = freemedforms.forms.item("Inter::Header");
+        bz_exoHeaderUi = ui.findChild("exoHeaderLineEdit");
+        bz_exoHeaderItem = freemedforms.forms.item("Exo::Header");
         bz_warningUi = ui.findChild("warningLineEdit");
-        //bz_warningItem = freemedforms.forms.item("Warning");
-        bz_affectionExoneranteHeaderTextEdit = ui.findChild("affectionExoneranteHeaderTextEdit");
-        bz_maladiesIntercurrentesHeaderTextEdit = ui.findChild("maladiesIntercurrentesHeaderTextEdit");
         bz_bizoneCollapsibleGroupItem = freemedforms.forms.item("Bizone::Group");
         bz_bizoneCollapsibleGroupUi = ui.findChild("bizoneGroupBox");
         bz_frequentExoUi = ui.findChild("exoFrequentListView");
+        bz_frequentExoItem = freemedforms.forms.item("Frequent::Exo");
         bz_hematoExoUi= ui.findChild("hemato_exo");
+        bz_hematoExoItem = freemedforms.forms.item("Basics::Hematology::Exo");
         bz_coagExoUi= ui.findChild("coag_exo");
+        bz_coagExoItem = freemedforms.forms.item("Basics::Coagulation::Exo");
         bz_electrolytesExoUi = ui.findChild("electrolytes_exo");
+        bz_electrolytesExoItem = freemedforms.forms.item("Basics::Electrolytes::Exo");
         bz_kidneyExoUi = ui.findChild("kidney_exo");
+        bz_kidneyExoItem = freemedforms.forms.item("Basics::Kidney::Exo");
         bz_protsExoUi = ui.findChild("prots_exo");
+        bz_protsExoItem = freemedforms.forms.item("Basics::Proteins::Exo");
         bz_liverExoUi = ui.findChild("liver_exo");
+        bz_liverExoItem = freemedforms.forms.item("Basics::Liver::Exo");
         bz_metabolicExoUi = ui.findChild("metabolic_exo");
+        bz_metabolicExoItem = freemedforms.forms.item("Basics::Metabolic::Exo");
         bz_tumorExoUi = ui.findChild("tumor_exo");
+        bz_tumorExoItem = freemedforms.forms.item("Basics::Cancer::Exo");
         bz_ironExoUi = ui.findChild("iron_exo");
+        bz_ironExoItem = freemedforms.forms.item("Basics::Iron::Exo");
         bz_prostateExoUi = ui.findChild("prostate_exo");
+        bz_prostateExoItem = freemedforms.forms.item("Basics::Prostate::Exo");
         bz_vitaminsExoUi = ui.findChild("vitamins_exo");
+        bz_vitaminsExoItem = freemedforms.forms.item("Basics::Vitamins::Exo");
         bz_biermerExoUi = ui.findChild("biermer_exo");
+        bz_biermerExoItem = freemedforms.forms.item("Basics::Biermer::Exo");
         bz_thyroidExoUi = ui.findChild("thyroid_exo");
+        bz_thyroidExoItem = freemedforms.forms.item("Basics::Thyroid::Exo");
         bz_serologiesExoUi = ui.findChild("serologies_exo");
+        bz_serologiesExoItem = freemedforms.forms.item("Basics::Serologies::Exo");
         bz_prExoUi = ui.findChild("pr_exo");
-
+        bz_prExoItem = freemedforms.forms.item("Basics::RhumatoidPolyarth::Exo");
         bz_warningUi.visible = false;
+        //bz_onCheckChanged();
     }
 
 function bz_connectUiElements()
     {
+        bz_bizoneCollapsibleGroupUi['toggled(bool)'].connect(this, bz_getUiElements);
         bz_bizoneCollapsibleGroupUi['toggled(bool)'].connect(this, bz_onCheckChanged);
-        //bz_bizoneCheckUi['stateChanged(int)'].connect(this, bz_onCheckChanged);
+
+    //bz_bizoneCheckUi['stateChanged(int)'].connect(this, bz_onCheckChanged);
     }
 
 function bz_onCheckChanged()
 {
-    print("function bz_onCheckChanged()");
-    print(bz_warningUi);
-    print(bz_warningUi.visible);
-    bz_warningUi.visible = false;
-    print(bz_affectionExoneranteItem.currentText);
-    bz_affectionExoneranteItem = freemedforms.forms.item("Affection::Exonerante");
-    print("freemedforms.uiTools.selectedItems(bz_frequentExoUi); -> ", freemedforms.uiTools.selectedItems(bz_frequentExoUi));
-    bz_getUiElements();
-
-    //print("bz_bizoneCollapsibleGroupItem", bz_bizoneCollapsibleGroupItem);
-    //print("bz_bizoneCollapsibleGroupItem.currentUuid", bz_bizoneCollapsibleGroupItem.currentUuid);
-    //print("bz_bizoneCollapsibleGroupItem.enabled", bz_bizoneCollapsibleGroupItem.enabled);
-    //print("bz_bizoneCollapsibleGroupItem.checked", bz_bizoneCollapsibleGroupItem.checked);
-    //print("bz_bizoneCollapsibleGroupItem.currentValue", bz_bizoneCollapsibleGroupItem.currentValue);
-    //print("bz_bizoneCollapsibleGroupItem.currentText", bz_bizoneCollapsibleGroupItem.currentText);
-    //print ("bz_bizoneCheckItem.enabled: ", bz_bizoneCheckItem.enabled);
-    //print ("bz_bizoneCheckItem.checked: ", bz_bizoneCheckItem.checked);
-    //print ("bz_bizoneCheckItem.currentUuid: ", bz_bizoneCheckItem.currentUuid);
-    //print ("bz_bizoneCheckItem.currentValue: ", bz_bizoneCheckItem.currentValue);
-    //print ("bz_bizoneCheckItem.currentText: ", bz_bizoneCheckItem.currentText);
-    //print ("typeof bz_bizoneCheckItem.checked", typeof bz_bizoneCheckItem.check);
     var checkValue = bz_bizoneCheckItem.checked;
+
+    bz_warningUi.visible = false;
+    bz_affectionExoneranteItem = freemedforms.forms.item("Affection::Exonerante");
+
     if (checkValue === false) {
         print("checkValue: ",checkValue);
-        bz_headerItem.currentText = "print";
-        bz_affectionExoneranteHeaderTextEdit.visible = false;
-        bz_maladiesIntercurrentesHeaderTextEdit.visible = false;
+        bz_interHeaderItem.currentText = "print";
+        bz_exoHeaderItem.currentText = "";
+        //bz_affectionExoneranteHeaderTextEdit.visible = false;
+        //bz_maladiesIntercurrentesHeaderTextEdit.visible = false;
         bz_warningUi.visible = false;
 
         bz_current = bz_maladiesIntercurrentesItem.currentText;
@@ -138,27 +176,25 @@ function bz_onCheckChanged()
         // if (ALD order is not empty and warning lineedit is empty),
         // display a warning
         // Warning: empty TextEditor item currentText property returns "undefined"
-        print("(freemedforms.uiTools.selectedItems(bz_frequentExoUi) === \"\"",
-                    (freemedforms.uiTools.selectedItems(bz_frequentExoUi) === ""));
-        print("(freemedforms.uiTools.selectedItems(bz_frequentExoUi) == \"\"",
-                    (freemedforms.uiTools.selectedItems(bz_frequentExoUi) == ""));
         var warn;
-        warn = !((freemedforms.uiTools.selectedItems(bz_frequentExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_hematoExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_coagExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_electrolytesExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_kidneyExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_protsExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_liverExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_metabolicExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_tumorExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_ironExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_prostateExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_vitaminsExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_biermerExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_thyroidExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_serologiesExoUi) == "")
-                && (freemedforms.uiTools.selectedItems(bz_prExoUi) == "")
+        var warn;
+
+        warn = !((typeof bz_frequentExoItem.currentUuid === "undefined")
+                && (typeof bz_hematoExoItem.currentUuid === "undefined")
+                && (typeof bz_coagExoItem.currentUuid === "undefined")
+                && (typeof bz_electrolytesExoItem.currentUuid === "undefined")
+                && (typeof bz_kidneyExoItem.currentUuid === "undefined")
+                && (typeof bz_protsExoItem.currentUuid === "undefined")
+                && (typeof bz_liverExoItem.currentUuid === "undefined")
+                && (typeof bz_metabolicExoItem.currentUuid === "undefined")
+                && (typeof bz_tumorExoItem.currentUuid === "undefined")
+                && (typeof bz_ironExoItem.currentUuid === "undefined")
+                && (typeof bz_prostateExoItem.currentUuid === "undefined")
+                && (typeof bz_vitaminsExoItem.currentUuid === "undefined")
+                && (typeof bz_biermerExoItem.currentUuid === "undefined")
+                && (typeof bz_thyroidExoItem.currentUuid === "undefined")
+                && (typeof bz_serologiesExoItem.currentUuid === "undefined")
+                && (typeof bz_prExoItem.currentUuid === "undefined")
                 && (typeof bz_affectionExoneranteItem.currentText === "undefined"));
 
         if (warn) {
@@ -168,18 +204,13 @@ function bz_onCheckChanged()
             }
         }
      } else {
-        print ("checkValue: ", checkValue);
         // delete warning about the need to delete ALD order
-        //bz_warningItem.currentText = "";
         bz_warningUi.setText("");
         bz_warningUi.visible = false;
 
-        // delete the headerItem that triggers printing of non ALD order header
-        bz_headerItem.currentText = "";
-
-        // hide ALD headers
-        bz_affectionExoneranteHeaderTextEdit.visible = true;
-        bz_maladiesIntercurrentesHeaderTextEdit.visible = true;
+        // delete the interHeaderItem that triggers printing of non ALD (inter) order header
+        bz_interHeaderItem.currentText = "";
+        bz_exoHeaderItem.currentText = "print";
         }
 }
 
@@ -189,25 +220,10 @@ function retranslateUi() {
 }
 
 
-/*!
-  This function is called whenever the user changes the text of "intercurrentes maladies"
-  If bizone is checked, set header item to empty string to prevent printing of normal header order
-  If bizone is not checked, set header item to "print" to trigger printing of normal order header
-*/
-function bz_header()
-{
-    var checkValue = bz_bizoneCheckItem.checked;
-    if (checkValue === false) {
-        bz_headerItem.currentText = "print";
-    } else {
-        bz_headerItem.currentText = "";
-    }
-}
-
 });
 
 // Setup Ui
-namespace.com.freemedforms.prescriptions.labtests.fr.bizone.setupUi();
+namespace.io.freehealth.prescriptions.labtests.fr.bizone.setupUi();
 
 
 
